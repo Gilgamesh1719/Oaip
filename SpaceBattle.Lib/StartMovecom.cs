b5edf4c37f90e073acd7196b1800008ad7cb62ef
Gilgamesh1719 <ltnkmk@gmail.com>
2023-02-23 20:00:26 +0600

    FixedLAB2

--- SpaceBattle.Lib/StartMovecom.cs
+++ SpaceBattle.Lib/StartMovecom.cs
@@ -6,10 +6,10 @@ public class StartMovecom: Icomand{
     public void execute()
     {
         foreach(var a in start.slov.ToList()){
-            IoC.Resolve<Icomand>("Uniobject.set", this.start.obj2, a.Key, a.Value);
+            IoC.Resolve<Icomand>("Uniobject.set", this.start.obj2, a.Key, a.Value).execute();
         }
         var cmd = IoC.Resolve<Icomand>("Getmove", this.start);
-        IoC.Resolve<Icomand>("Uniobject.set", this.start.obj2, "LongMove", cmd);
-        IoC.Resolve<Icomand>("Push", cmd);
+        IoC.Resolve<Icomand>("Uniobject.set", this.start.obj2, "LongMove", cmd).execute();
+        IoC.Resolve<Icomand>("Push", cmd).execute();
     }
 }
